57d7aa65dcd445ede13e28ffc0f6947771f82db7
Phong Tran <tranphongbb@icloud.com>
2020-06-28 12:48:45 +0700

    add new content

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,111 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('created');
+document.addEventListener("DOMContentLoaded", () => {
+    //DOMContentLoaded la cai de khi ma DOM duoc load len, thi no se lam gi.
+    //O ben jquery thi la $.document.ready hoac $(function())
+
+    //goi render vao day
+    //
     render();
-    console.log('mounted');
-})
+});
 
 let users = [
     {
         name: "Phong Tran",
-        email: "tranphongbb@outlook.com"
+        email: "tranphongbb@outlook.com",
     },
     {
         name: "Thao Pham",
-        email: "pphuongthao22@gmail.com"
-    }
+        email: "pphuongthao22@gmail.com",
+    },
 ];
 
-if (localStorage.getItem('users') === null) {
-    localStorage.setItem('users', JSON.stringify(users));
-}
-
 function render() {
-    let storedUsers = JSON.parse(localStorage.getItem('users'));
-    for (let i = 0; i < storedUsers.length; i++) {
-        let user = storedUsers[i];
-        let table = "<tr>" +
-            "<td>"+ user.name  +"</td>" +
-            "<td>"+ user.email +"</td>" +
-            "<td><button class='ui negative basic button' onclick='deleteUser("+ i +")'>Delete</button></td>" +
-            "</tr>";
-        document.getElementById('dataTable').insertAdjacentHTML('beforeend', table);
+    //loop qua cai list object
+    for (let i = 0; i < users.length; i++) {
+        //cai nay don gian ma =))\
+        let user = users[i];
+        let htmlString =
+            "<tr>" +
+            "<td>" +
+            user.name +
+            "</td>" +
+            "<td>" +
+            user.email +
+            "</td>" +
+            "<td><button class='ui negative basic button' onclick='deleteUser(" +
+            i +
+            ")'>Delete</button></td>";
+        ("</tr>");
+
+        document.getElementById("dataTable").insertAdjacentHTML("beforeend", htmlString);
     }
 }
 
 function addNewUser() {
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
+    //lay value tu o input id name
+    let name = document.getElementById("name").value;
+    //lay value tu o input email
+    let email = document.getElementById("email").value;
 
+    //tao 1 object user moi, vi key va value cua chung no nhu nhau nen viet kieu nay luon nhe, tuc la:
+    //name: name => name thoi dc roi
     let newUser = {
         name,
-        email
+        email,
     };
 
+    //tao xong object moi nhe
+    //add no vao list o tren dau nhe
+
     users.push(newUser);
-    localStorage.setItem('users', JSON.stringify(users));
-    document.getElementById('dataTable').innerHTML = '';
+
+    //next, render lai list user, bang cach goi ham render thoi
+    //vi phai render lai toan bo list, nen can phai clear cai body cua selector cu di, render cai moi
+    document.getElementById("dataTable").innerHTML = "";
     render();
 }
 
 function deleteUser(index) {
     users.splice(index, 1);
-    localStorage.setItem('users', JSON.stringify(users));
-    document.getElementById('dataTable').innerHTML = '';
+
+    document.getElementById("dataTable").innerHTML = "";
     render();
 }
+
+
+
+const app = new Vue({
+    //mock up
+    el: '#app',
+    //initial cai data, nhu cach minh tao 1 list object nhu o tren nhe
+    data: {
+        users: [
+            {
+                name: 'Phong Tran',
+                email: 'tranphongbb@outlook.com'
+            },
+            {
+                name: 'Ha Dam',
+                email: 'abc@gmail.com'
+            }
+        ],
+        name: 'Hoac neu toi thich co du lieu khoi tao thi sao?',
+        email: ''
+    },
+    methods: {
+        addNewUser() {
+            let newUser = {
+                name: this.name,
+                email: this.email
+            };
+
+            this.users.push(newUser);
+        },
+        deleteUser(index) {
+            this.users.splice(index, 1);
+        }
+    }
+})
+
+
+
+
