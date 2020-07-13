023ac1611eca7fc858049ac0a8a7c8e3007b6e48
Phong Tran <tranphongbb@icloud.com>
2020-07-13 18:19:19 +0700

    new webpack config

---
+++ config/webpack.common.config.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const HtmlPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const helpers = require("./helpers");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const commonWebpackConfig = {
+    entry: {
+        polyfill: "@babel/polyfill",
+        js: helpers.root("src", "js", "index"),
+    },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            "@": helpers.root("src"),
+        },
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, loader: "babel-loader", include: [helpers.root("src")] },
+            {
+                test: /\.css$/,
+                use: [
+                    isDevelopment ? "style-loader" : MiniCSSExtractPlugin.loader,
+                    { loader: "css-loader", options: { sourceMap: isDevelopment } },
+                    "postcss-loader",
+                ],
+            },
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass"),
+                            sassOptions: {
+                                fiber: require("fibers"),
+                                indentedSyntax: true, // optional
+                            },
+                        },
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: "postcss",
+                            plugins: [require("tailwindcss"), require("autoprefixer")],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlPlugin({
+            template: "./src/index.html",
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true,
+            },
+        }),
+    ],
+};
+
+module.exports = commonWebpackConfig;
